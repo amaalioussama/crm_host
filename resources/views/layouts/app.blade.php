54e0f22effbac913212ce11ef2a5f952d7bbbeec
unknown <amaaliousama771@gmail.com>
2025-03-17 23:34:18 +0000

    fin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-<script src="{{ mix('js/app.js') }}"></script>
+        <link href="{{('css/app.css') }}" rel="stylesheet">
+<script src="{{ ('js/app.js') }}"></script>
 <link href="https://cdn.jsdelivr.net/npm/flag-icon-css/css/flag-icon.min.css" rel="stylesheet">
 
 
